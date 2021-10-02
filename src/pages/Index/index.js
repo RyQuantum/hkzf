644253313913cb57b798a79e159f6f476e2807c5
Ryan <ryan@rently.com>
2021-10-02 23:56:57 +0800

    add carousel

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,7 +1,43 @@
 import React from 'react'
+import { Carousel } from "antd-mobile";
 
 export default class Index extends React.Component {
-    render() {
-        return <div>This is home</div>
-    }
+  state = {
+    data: ['1', '2', '3'],
+  }
+
+  componentDidMount() {
+    // simulate img loading
+    setTimeout(() => {
+      this.setState({
+        data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
+      });
+    }, 100);
+  }
+
+  render() {
+    return (
+      <div>
+        <Carousel
+          autoplay
+          infinite
+          autoplayInterval={5000}
+        >
+          {this.state.data.map(val => (
+            <a
+              key={val}
+              href="http://incognito.tpddns.cn:666"
+              style={{ display: 'inline-block', width: '100%', height: 212 }}
+            >
+              <img
+                src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+                alt=""
+                style={{ width: '100%', verticalAlign: 'top' }}
+              />
+            </a>
+          ))}
+        </Carousel>
+      </div>
+    );
+  }
 }
