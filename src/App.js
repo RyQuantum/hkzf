27b2590300f1b538b30608f51f9600401c10a875
Ryan <ryan@rently.com>
2021-10-04 14:47:29 +0800

    add Baidu Map

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
-import {Button} from 'antd-mobile';
 
 import Home from './pages/Home'
 import CityList from './pages/CityList'
+import Map from './pages/Map'
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
         <Route path="/" exact render={() => <Redirect to="/home" />} />
         <Route path="/home" component={Home}/>
         <Route path="/cityList" component={CityList}/>
+        <Route path="/map" component={Map}/>
       </div>
     </Router>
   );
