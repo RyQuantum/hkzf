cf0789eca8f1f6ac5fa217c659c7d4fb1809b252
mayue3434 <ryan@rently.com>
2021-10-08 01:26:41 +0800

    add news page

--- hkzf-mobile/src/pages/News/index.js
+++ hkzf-mobile/src/pages/News/index.js
@@ -1,10 +1,52 @@
 import React from 'react'
+import axios from 'axios'
+import { Flex, WingBlank } from 'antd-mobile'
 
 export default class News extends React.Component {
+  state = {
+    news: [],
+  }
+
+  componentDidMount() {
+    this.getNews()
+  }
+
+  async getNews() {
+    const res = await axios.get(
+      'http://incognito.tpddns.cn:8081/home/news?area=AREA%7C88cff55c-aaa4-e2e0'
+    )
+
+    this.setState({
+      news: res.data.body
+    })
+  }
+
+  renderNews() {
+    return this.state.news.map(item => (
+      <div className="news-item" key={item.id}>
+        <div className="imgwrap">
+          <img
+            className="img"
+            src={`http://incognito.tpddns.cn:8081${item.imgSrc}`}
+            alt=""
+          />
+        </div>
+        <Flex className="content" direction="column" justify="between">
+          <h3 className="title">{item.title}</h3>
+          <Flex className="info" justify="between">
+            <span>{item.from}</span>
+            <span>{item.date}</span>
+          </Flex>
+        </Flex>
+      </div>
+    ))
+  }
+
   render() {
     return (
-      <div style={{ backgroundColor: 'green' }}>
-        这是 News组件 的内容，它是子路由的内容
+      <div className="news">
+        <h3 className="group-title">News</h3>
+        <WingBlank size="md">{this.renderNews()}</WingBlank>
       </div>
     )
   }
